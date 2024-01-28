5ad922ab2482fdc19fb14a6221a0eb54d9face3c
Isaac Berrio <berriisaax18@hotmail.com>
2024-01-28 13:17:53 -0500

    Add: bookshop react-router.dom; Update: Navbar.jsx, App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,16 +3,21 @@ import Navbar from "../components/Navbar";
 import Home from "../pages/Home";
 import CreateCountry from "../pages/CreateCountry";
 import "../style/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <main className="container">
-      <Navbar/>
-      <Home/>
-      <Footer/>
-      <CreateCountry />
-    </main>
+    <BrowserRouter>
+      <main className="container">
+        <Navbar />
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-country" element={<CreateCountry />} />
+        {/* <Route path="/manage-country" element={<ManageCountry />} /> */}
+        </Routes>
+        <Footer />
+      </main>
+    </BrowserRouter>
   );
 }
 
